11f6edf3a66e7e5fcad37e681fc34f22a60feb6d
yj411511168 <50351635+yj411511168@users.noreply.github.com>
2024-10-28 17:56:17 +0800

    update ship_move_speed and bordercontrol

--- alien_invation/alien_invasion.py
+++ alien_invation/alien_invasion.py
@@ -20,20 +20,48 @@ class AlienInvasion:
 		#设置背景色
 		self.bg_color = self.settings.bg_color
 
+	def _check_keydown_events(self, event):
+		"""响应按下"""
+		if event.key == pygame.K_RIGHT:
+			self.ship.moving_right = True
+		elif event.key == pygame.K_LEFT:
+			self.ship.moving_left = True
+		elif event.key == pygame.K_q:
+			sys.exit()
+
+	def _check_keyup_events(self, event):
+		"""响应松开"""
+		if event.key == pygame.K_RIGHT:
+			self.ship.moving_right = False
+		elif event.key == pygame.K_LEFT:
+			self.ship.moving_left = False
+
+	def _check_events(self):
+		"""相应按键和鼠标事件"""
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				sys.exit()
+			elif event.type == pygame.KEYDOWN:
+				self._check_keydown_events(event)
+			elif event.type == pygame.KEYUP:
+				self._check_keyup_events(event)
+
+	def _update_screen(self):
+		"""更新屏幕上的图形，并切换到新屏幕"""
+		self.screen.fill(self.settings.bg_color)
+		self.ship.blitme()
+
+		pygame.display.flip()
+
+
 	def run_game(self):
 		"""开始游戏主循环"""
 		while True:
 			# 侦听键盘和鼠标事件
-			for event in pygame.event.get():
-				if event.type == pygame.QUIT:
-					sys.exit()
-
-			# 每次循环都会重回屏幕
-			self.screen.fill(self.bg_color)
-			self.ship.blitme()
-
-			# 让最近绘制的屏幕可见
-			pygame.display.flip()
+			self._check_events()
+			self.ship.update()
+			# 每次循环都会重绘屏幕
+			self._update_screen()
 			self.clock.tick(60)
 
 if __name__ == '__main__':
--- alien_invation/settings.py
+++ alien_invation/settings.py
@@ -8,3 +8,6 @@ class Settings:
 		self.screen_height = 800
 		self.bg_color = (230, 230, 230)
 
+		# 飞船设置
+		self.ship_speed = 1.5
+
