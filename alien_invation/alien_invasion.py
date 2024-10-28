f0de263bb561ed66c5287678851c829784cb2ef1
yj411511168 <50351635+yj411511168@users.noreply.github.com>
2024-10-28 21:09:42 +0800

    add bullet_class and fire mathod

--- alien_invation/alien_invasion.py
+++ alien_invation/alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
 	"""管理游戏资源和行为的类"""
@@ -16,6 +17,7 @@ class AlienInvasion:
 											   self.settings.screen_height))
 		pygame.display.set_caption("Alien Invasion")
 		self.ship = Ship(self)
+		self.bullets = pygame.sprite.Group()
 
 		#设置背景色
 		self.bg_color = self.settings.bg_color
@@ -26,6 +28,8 @@ class AlienInvasion:
 			self.ship.moving_right = True
 		elif event.key == pygame.K_LEFT:
 			self.ship.moving_left = True
+		elif event.key == pygame.K_SPACE:
+			self._fire_bullet()
 		elif event.key == pygame.K_q:
 			sys.exit()
 
@@ -46,9 +50,26 @@ class AlienInvasion:
 			elif event.type == pygame.KEYUP:
 				self._check_keyup_events(event)
 
+	def _fire_bullet(self):
+		"""创建一颗子弹，并将其加入到编组bullets"""
+		if len(self.bullets) < self.settings.bullets_allowed:
+			new_bullet = Bullet(self)
+			self.bullets.add(new_bullet)
+
+	def _update_bullets(self):
+		"""更新子弹的位置和删除已消失的子弹"""
+		self.bullets.update()
+
+		# 删除已消失的子弹
+		for bullet in self.bullets.copy():
+			if bullet.rect.bottom <= 0:
+				self.bullets.remove(bullet)
+
 	def _update_screen(self):
 		"""更新屏幕上的图形，并切换到新屏幕"""
 		self.screen.fill(self.settings.bg_color)
+		for bullet in self.bullets:
+			bullet.draw_bullet()
 		self.ship.blitme()
 
 		pygame.display.flip()
@@ -60,6 +81,7 @@ class AlienInvasion:
 			# 侦听键盘和鼠标事件
 			self._check_events()
 			self.ship.update()
+			self._update_bullets()
 			# 每次循环都会重绘屏幕
 			self._update_screen()
 			self.clock.tick(60)
--- alien_invation/settings.py
+++ alien_invation/settings.py
@@ -11,3 +11,10 @@ class Settings:
 		# 飞船设置
 		self.ship_speed = 1.5
 
+		# 子弹设置
+		self.bullet_speed = 3.0
+		self.bullet_width = 3
+		self.bullet_height = 15
+		self.bullet_color = (60, 60, 60)
+		self.bullets_allowed = 6
+
