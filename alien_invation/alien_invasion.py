75d2b4bec7f79f9a37b8482259d88d3345ac3330
yj411511168 <50351635+yj411511168@users.noreply.github.com>
2024-10-28 11:20:19 +0800

    add_ship_class and bmp

--- alien_invation/alien_invasion.py
+++ alien_invation/alien_invasion.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
 	"""管理游戏资源和行为的类"""
@@ -14,6 +15,7 @@ class AlienInvasion:
 		self.screen = pygame.display.set_mode((self.settings.screen_width,
 											   self.settings.screen_height))
 		pygame.display.set_caption("Alien Invasion")
+		self.ship = Ship(self)
 
 		#设置背景色
 		self.bg_color = self.settings.bg_color
@@ -28,6 +30,7 @@ class AlienInvasion:
 
 			# 每次循环都会重回屏幕
 			self.screen.fill(self.bg_color)
+			self.ship.blitme()
 
 			# 让最近绘制的屏幕可见
 			pygame.display.flip()
